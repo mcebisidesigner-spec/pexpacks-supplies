0d6f211430218aaa23ba59c51d5f5bf52e1ba8c2
McebisiH <mcebisidesigner@gmail.com>
2026-05-14 12:20:27 +0200

    push

--- lib/brand-assets.ts
+++ lib/brand-assets.ts
@@ -1,8 +1,10 @@
 import { siteUrl } from "@/lib/seo";
 
+const brandLogoVersion = "20260514";
+
 export const brandLogoPaths = {
-  default: "/images/logo.svg",
-  white: "/images/logo-white.svg",
+  default: `/images/logo.svg?v=${brandLogoVersion}`,
+  white: `/images/logo-white.svg?v=${brandLogoVersion}`,
 } as const;
 
 export type BrandLogoVariant = keyof typeof brandLogoPaths;
